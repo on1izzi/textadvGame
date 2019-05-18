b18e02ff5fcd0893971c09e5c72b900161c2ba22
Vladthemir <47650566+vladthemir@users.noreply.github.com>
2019-05-18 12:52:54 +0100

    Update TextAdventureGame.hs

--- TextAdventureGame.hs
+++ TextAdventureGame.hs
@@ -12,39 +12,40 @@ instructions =
 
     "i                  -- to see your inventory (what you are holding).\n" ++
     "quit               -- to end the game and quit.\n"++
-    "Input which direction "
+    "Input which direction you want to take "
 game :: IO ()
 game = do putStrLn instructions
           direction <- getLine
-          putStrLn "input which room are you going to enter:"
+          putStrLn "input which road you want to start your adventure:"
           play direction
   
 play :: String -> IO ()
 play direction =
    do guess <- getLine
       if guess == "quit" then
-         putStrLn "You won/game over!"
+         putStrLn "game over!"
       else
          do putStrLn (match direction guess)
-            play direction
+            play direction 
 
 match :: String -> String -> String
 match xs ys = description xs ys
 
---need to add more options of the rooms, for example
--- description "s" "room4"= "you are inside room4"
+
 description :: String->String -> String
-description "w" "room1" = "You are in a room1.  To the north is the mouth\n" ++
-    "of a room2; to the south is a small room5. Your\n" ++
-	"assignment, should you decide to accept it, is to\n" ++
-	"recover the ruby and return it to\n" ++
-	"this room3."
-description "w" "room2" = 
-    "Congratulations!!  You have recovered the ruby and won the game."
-description "s" "room1" =
-    "You are in the mouth of a dank room2.  The exit is to\n" ++
-    "the south; there is a large, dark, round passage to\n" ++
-    "the east."
-description _ "s room4"= "you are inside s room4"
-description _ "n room4"= "you are inside n room4"
-description _ _ = "you can't see anything."
+description "w" "road1" = "You are on road1.\n" ++
+    "To the north is the gas station\n" ++
+    "behind the gas station there is road2;\n"++
+    "to the south is a small road5.\n" ++
+    "Your assignment, is to\n" ++
+    "recover the car\n" ++
+    ""
+description "w" "road2" = 
+    "Congratulations!!  You have recovered the car, you are a winner!!!"
+description "w" "road3" =
+    "Go and get my car!!!"
+description _ "s road4"= "your direction is south on road4"
+
+description _ "n road4"= "your direction is north on road4"
+
+description _ _ = "this road is not avalable."
